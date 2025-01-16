59c1210e0c9a1c6fb85253d3912bbfe7094d3588
KemraDragon <94020099+KemraDragon@users.noreply.github.com>
2025-01-16 09:07:57 -0800

    Initial Commit

---
+++ Eartquake-Api/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Eartquake-Api
+//
+//  Created by Kevin  on 16-01-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eartquake-Api/Eartquake_ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eartquake_ApiApp.swift
+//  Eartquake-Api
+//
+//  Created by Kevin  on 16-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Eartquake_ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
